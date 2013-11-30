4245b64b52956dfd52731db2fa75e91507d84874
Akshay <aktwo@princeton.edu>
2013-11-30 14:48:18 -0500

    Updated server.js to use express instead of connect and implemented bare-bones IP address filtering

--- server.js
+++ server.js
@@ -1,11 +1,29 @@
 var io = require('socket.io'),
-connect = require('connect'),
+express = require('express'),
 chatter = require('./chatter.js');
 
 var port = process.env.PORT || 3000;
+var app = express();
 
-var app = connect().use(connect.static('public')).listen(port);
-var chatRoom = io.listen(app);
+// FIXME change this to actual princeton IP addresses
+var regExpArray = new Array(/127.0.0.1/);
+
+app.get('/', function(req, res) {
+
+for (var i = 0; i < regExpArray.length; i++) {
+  if(regExpArray[i].test(req.ip)) {
+    res.sendfile(__dirname + '/public/index.html');
+  }
+  else {
+    res.send('Sorry, this site is only for Princeton students!');
+  }
+}
+
+});
+
+app.use(express.static(__dirname + '/public'));
+
+var chatRoom = io.listen(app.listen(port));
 
 chatter.setSockets(chatRoom.sockets);
 
