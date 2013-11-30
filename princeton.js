a12bc9881c11a218484c083f912a4980b664a2a1
Dan Kang <dan@dskang.com>
2013-11-30 16:04:54 -0500

    split princeton utils into separate module

---
+++ princeton.js
@@ -0,0 +1,24 @@
+var range_check = require('range_check');
+
+var princetonIPs = [
+  "128.112.0.0/16",
+  "140.180.0.0/16",
+  "204.153.48.0/22",
+  "66.180.176.0/24",
+  "66.180.177.0/24",
+  "66.180.180.0/22"
+];
+
+var isValidIP = function (userIP) {
+  if (userIP == "127.0.0.1") {
+    return true;
+  }
+  for (var i = 0; i < princetonIPs.length; i++) {
+    if (range_check.in_range(userIP, princetonIPs[i])) {
+      return true;
+    }
+  }
+  return false;
+}
+
+exports.isValidIP = isValidIP;
--- server.js
+++ server.js
@@ -1,34 +1,13 @@
 var io = require('socket.io'),
 express = require('express'),
-range_check = require('range_check'),
+princeton = require('./princeton'),
 chatter = require('./chatter.js');
 
 var port = process.env.PORT || 3000;
 var app = express();
 
-var princetonIPs = [
-  "128.112.0.0/16",
-  "140.180.0.0/16",
-  "204.153.48.0/22",
-  "66.180.176.0/24",
-  "66.180.177.0/24",
-  "66.180.180.0/22"
-];
-
-function isValidIP(userIP) {
-  if (userIP == "127.0.0.1") {
-    return true;
-  }
-  for (var i = 0; i < princetonIPs.length; i++) {
-    if (range_check.in_range(userIP, princetonIPs[i])) {
-      return true;
-    }
-  }
-  return false;
-}
-
 app.get('/', function(req, res) {
-  if (isValidIP(req.ip)) {
+  if (princeton.isValidIP(req.ip)) {
     res.sendfile(__dirname + '/public/index.html');
   } else {
     res.send('Sorry, this site is only for Princeton students!');
@@ -42,7 +21,7 @@ var chatRoom = io.listen(app.listen(port));
 chatter.setSockets(chatRoom.sockets);
 
 chatRoom.sockets.on('connection', function (socket) {
-  if (isValidIP(socket.handshake.address.address)) {
+  if (princeton.isValidIP(socket.handshake.address.address)) {
     chatter.connectChatter(socket);
   }
 });
