74a5759e03504bfa6d8bf74806be9bab9f53b04d
Dan Kang <dan@dskang.com>
2014-01-09 22:49:18 -0500

    rename variables

--- chatter.js
+++ chatter.js
@@ -1,17 +1,17 @@
 // FIXME: make this larger than 0
 var threshold = 0;
 var queue = require('./queue.js');
-var myName = 'You';
-var theirName = 'Anonymous Tiger';
+var userName = 'You';
+var partnerName = 'Anonymous Tiger';
 var conversation = require('./conversation.js');
 
 // FIXME: make this false before pushing to production
 var debug = true;
 
-exports.connectChatter = function (currentSocket, userID) {
+exports.connectChatter = function(currentSocket, userID) {
 
-  var thatUser;
-  var thisUser = {
+  var partner;
+  var user = {
     socket: currentSocket,
     ownID: userID,
     partnerID: null,
@@ -25,7 +25,7 @@ exports.connectChatter = function (currentSocket, userID) {
   };
 
   // Send welcome message to connected user.
-  thisUser.socket.emit('entrance', {
+  user.socket.emit('entrance', {
     message: 'Welcome to the chat room!'
   });
 
@@ -33,24 +33,24 @@ exports.connectChatter = function (currentSocket, userID) {
   if (queue.length() <= threshold) {
 
     // If the user isn't already in the buffer, add the user.
-    if (!queue.containsUser(thisUser) || debug) {
-      queue.addUser(thisUser);
-      thisUser.socket.emit('waiting', {
+    if (!queue.containsUser(user) || debug) {
+      queue.addUser(user);
+      user.socket.emit('waiting', {
         message: 'Waiting for partner to join.'
       });
-      thisUser.socket.on('disconnect', function() {
-        queue.removeUser(thisUser);
+      user.socket.on('disconnect', function() {
+        queue.removeUser(user);
       });
     }
 
     // Otherwise, serve an denial message and prevent the user from
     // connecting.
     else {
-      thisUser.socket.emit('exit', {
+      user.socket.emit('exit', {
         message: 'Sorry, you can only chat with one person at a time!'
       });
-      thisUser.socket.on('disconnect', function() {
-        queue.removeUser(thisUser);
+      user.socket.on('disconnect', function() {
+        queue.removeUser(user);
       });
     }
 
@@ -60,81 +60,81 @@ exports.connectChatter = function (currentSocket, userID) {
 
     // Only match the current user if that user isn't already present
     // in the buffer (to prevent people from chatting with themselves).
-    if (!queue.containsUser(thisUser) || debug) {
-      thatUser = queue.getPartner(thisUser);
+    if (!queue.containsUser(user) || debug) {
+      partner = queue.getPartner(user);
 
       // Populate the ID field of both users.
-      thatUser.partnerID = thisUser.ownID;
-      thisUser.partnerID = thatUser.ownID;
+      partner.partnerID = user.ownID;
+      user.partnerID = partner.ownID;
 
       // Populate startTime field of both users.
-      thatUser.startTime = Date.now();
-      thisUser.startTime = Date.now();
+      partner.startTime = Date.now();
+      user.startTime = Date.now();
 
       // Tell both users that they can start chatting.
       var connectedMessage = {
         message: 'Connected! Go ahead and start chatting.'
       };
 
-      thisUser.socket.emit('ready', connectedMessage);
-      thatUser.socket.emit('ready', connectedMessage);
+      user.socket.emit('ready', connectedMessage);
+      partner.socket.emit('ready', connectedMessage);
 
       var disconnectedMessage = {
-        message: theirName + ' has disconnected. Refresh the page to start another chat!'
+        message: partnerName + ' has disconnected. Refresh the page to start another chat!'
       };
 
       // When either of the users disconnects from the chatroom, log
       // the conversation and let the other user know that a disconnect
       // has occurred.
-      thisUser.socket.on('disconnect', function() {
-        thatUser.socket.emit('exit', disconnectedMessage);
-        conversation.save(thatUser);
+      user.socket.on('disconnect', function() {
+        partner.socket.emit('exit', disconnectedMessage);
+        conversation.save(partner);
 
       });
 
-      thatUser.socket.on('disconnect', function() {
-        thisUser.socket.emit('exit', disconnectedMessage);
-        conversation.save(thisUser);
+      partner.socket.on('disconnect', function() {
+        user.socket.emit('exit', disconnectedMessage);
+        conversation.save(user);
       });
 
       // Whenever either user sends a message, update the log of messages
       // sent/received and display the message to both users.
-      thisUser.socket.on('chat', function(data) {
+      user.socket.on('chat', function(data) {
 
-        thisUser.messagesSent++;
-        thatUser.messagesReceived++;
+        user.messagesSent++;
+        partner.messagesReceived++;
 
-        thisUser.socket.emit('chat', {
-          message: myName + ': ' + data.message
+        user.socket.emit('chat', {
+          message: userName + ': ' + data.message
         });
-        thatUser.socket.emit('chat', {
-          message: theirName + ': ' + data.message
+        partner.socket.emit('chat', {
+          message: partnerName + ': ' + data.message
         });
       });
 
-      thatUser.socket.on('chat', function(data) {
+      partner.socket.on('chat', function(data) {
 
-        thatUser.messagesSent++;
-        thisUser.messagesReceived++;
+        partner.messagesSent++;
+        user.messagesReceived++;
 
-        thisUser.socket.emit('chat', {
-          message: theirName + ': ' + data.message
+        user.socket.emit('chat', {
+          message: partnerName + ': ' + data.message
         });
-        thatUser.socket.emit('chat', {
-          message: myName + ': ' + data.message
+        partner.socket.emit('chat', {
+          message: userName + ': ' + data.message
         });
       });
 
       // Whenever the "Reveal My Identity" button is revealed,
       // update the corresponding property of both users.
-      thisUser.socket.on('reveal-button', function(data) {
-        thisUser.buttonDisplayed = true;
-        thatUser.buttonDisplayed = true;
+      user.socket.on('reveal-button', function(data) {
+        user.buttonDisplayed = true;
+        partner.buttonDisplayed = true;
       });
 
-      thatUser.socket.on('reveal-button', function(data) {
-        thatUser.buttonDisplayed = true;
-        thisUser.buttonDisplayed = true;
+      partner.socket.on('reveal-button', function(data) {
+        partner.buttonDisplayed = true;
+        user.buttonDisplayed = true;
       })
     }
 
@@ -142,11 +142,11 @@ exports.connectChatter = function (currentSocket, userID) {
     // denial message to them and prevent them from re-entering the
     // buffer.
     else {
-      thisUser.socket.emit('exit', {
+      user.socket.emit('exit', {
         message: 'Sorry, you can only chat with one person at a time!'
       });
-      thisUser.socket.on('disconnect', function() {
-        queue.removeUser(thisUser);
+      user.socket.on('disconnect', function() {
+        queue.removeUser(user);
       });
     }
   }
