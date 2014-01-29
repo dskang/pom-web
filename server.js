d4e7a053cdd1d3c5e38097ceaa5ee3abdb8e951b
Dan Kang <dan@dskang.com>
2014-01-29 16:20:31 -0500

    set cookie domain properly

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var crypto = require('crypto'),
 
 var port = process.env.PORT || 3000;
 app.listen(port);
+console.log('Express listening on port ' + port);
 
 app.use(express.cookieParser());
 
@@ -12,11 +13,17 @@ app.get('/about', function(req, res) {
   res.sendfile(__dirname + '/public/about.html');
 });
 
+var cookieDomain;
+app.configure('production', function() {
+  cookieDomain = '.tigersanonymous.com';
+});
+
 app.get('/chat', function(req, res) {
   if (!req.cookies.chatterID) {
     crypto.pseudoRandomBytes(16, function(err, buff) {
      res.cookie('chatterID', buff.toString('hex'), {
-       maxAge: 60*60*24*356*1000
+       maxAge: 60*60*24*356*1000,
+       domain: cookieDomain
      });
    });
   }
